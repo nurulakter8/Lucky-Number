b6fe31f54ef13ce405a3ad759440202bbbda3906
Nurul <nurulakter8@gmail.com>
2021-06-04 18:52:15 -0500

    major button added

--- public/app.js
+++ public/app.js
@@ -1,11 +1,21 @@
 const buttonTag = document.getElementById('button-lucky');
+const majorButtonTag = document.getElementById('button-major');
+
 buttonTag.addEventListener('click', buttonClick);
+majorButtonTag.addEventListener('click', majorButtonClick);
+
+const messageTag = document.getElementById('message');
 
 function buttonClick (){
 	// console.log('button clicked');
 	const n1 = Math.floor(Math.random() * 100);
 	const n2 = Math.floor(Math.random() * 100);
-	const messageTag = document.getElementById('message');
+	
 	messageTag.innerHTML = 'Your lucky numbers are '
 		+ n1 + ' and ' + n2 + '.';
+}
+
+function majorButtonClick() {
+	messageTag.innerHTML = 'My major is CS.' ;
+	
 }
